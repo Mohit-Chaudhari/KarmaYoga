4703f2d624b4c595d7d9620e674a95217c432090
Mohit <mohit@L738.local>
2020-02-16 10:51:23 +0530

    Initialising KarmaYoga project.

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+
+const routes: Routes = [{ path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) }, { path: 'student', loadChildren: () => import('./student/student.module').then(m => m.StudentModule) }, { path: 'teacher', loadChildren: () => import('./teacher/teacher.module').then(m => m.TeacherModule) }];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
