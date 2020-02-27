813628a4269daec1b71d5f3bf9e057a7662dc465
Mohit <mohit@L738.local>
2020-02-27 22:33:08 +0530

    Header designed and router links appended to it.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 
 
 const routes: Routes = [
 { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) }, 
 { path: 'student', loadChildren: () => import('./student/student.module').then(m => m.StudentModule) }, 
-{ path: 'teacher', loadChildren: () => import('./teacher/teacher.module').then(m => m.TeacherModule) }];
+{ path: 'teacher', loadChildren: () => import('./teacher/teacher.module').then(m => m.TeacherModule) },
+{ path: 'contact-us', component: ContactUsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
